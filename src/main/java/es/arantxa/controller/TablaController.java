c4aadbca516d4b42b2e3349b36a16ea85f0649df
arantxa <149501497+arantxaMain@users.noreply.github.com>
2025-09-26 14:20:49 +0200

    FEAT: añado controlador para tabla.fxml

---
+++ src/main/java/es/arantxa/controller/TablaController.java
@@ -0,0 +1,38 @@
+package es.arantxa.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class TablaController  {
+
+    @FXML
+    private DatePicker dateBirth;
+
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    private TextField txtFirstName;
+
+    @FXML
+    private TextField txtLastName;
+
+    @FXML
+    void btnAddClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRestoreClick(ActionEvent event) {
+
+    }
+
+}
